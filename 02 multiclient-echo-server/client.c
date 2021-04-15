22e985dd859acab263dbb04241433d0498ab86bb
ceyhun <isaceyhunseker@gmail.com>
2021-04-15 16:39:09 +0300

    edit gitignore and removes false code blocks

--- 02 multiclient-echo-server/client.c
+++ 02 multiclient-echo-server/client.c
@@ -6,34 +6,3 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-#define PORT 2323
-#define BUFFER_LENGHT 1024
-
-void main(){
-	
-	int clientSocket;
-	struct sockaddr_in serverAddr;
-	char msgBuffer[BUFFER_LENGHT];
-
-	clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-	printf("[+]Client Socket Created Sucessfully.\n");
-
-	memset(&serverAddr, '\0', sizeof(serverAddr));
-	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_port = htons(PORT);
-	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-	connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
-	printf("[+]Connected to Server.\n");
-
-	while(1) {
-		printf("Enter a message, write 'qu' to close the chat: ");
-		fgets(msgBuffer, 1024, stdin);
-		send(clientSocket, msgBuffer, strlen(msgBuffer), 0);
-			if(msgBuffer[0] == 'q' && msgBuffer[1] == 'u'){
-				break;
-			}
-	}
-	printf("[+]Closing the connection.\n");
-	shutdown(clientSocket, SHUT_RDWR);
-}
--- 03 chat-app/server.c
+++ 03 chat-app/server.c
@@ -6,42 +6,4 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-#define PORT 2323
-#define BUFFER_LENGHT 1024
 
-void main(){
-
-	int sockfd;
-	struct sockaddr_in serverAddr;
-
-	int newSocket;
-	struct sockaddr_in newAddr;
-
-	socklen_t addr_size;
-	char buffer[BUFFER_LENGHT];
-
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	printf("[+]Server Socket Created Sucessfully.\n");
-	memset(&serverAddr, '\0', sizeof(serverAddr));
-
-	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_port = htons(PORT);
-	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-	bind(sockfd, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
-	printf("[+]Bind to Port number %d.\n", 2323);
-
-	listen(sockfd, 5);
-	printf("[+]Listening...\n");
-
-	while(newSocket = accept(sockfd, (struct sockaddr*)&newAddr, &addr_size)){
-		while (recv(newSocket, buffer, BUFFER_LENGHT, 0)>0) {
-			printf("Message Received: %s", buffer);
-			if(buffer[0] == 'q' && buffer[1] == 'u')
-				break;
-			memset(buffer, '\0', sizeof(char)*BUFFER_LENGHT);}
-		printf("[+]Closing the connection.\n");
-		shutdown(newSocket, SHUT_RDWR);
-		exit(0);
-	}
-}
