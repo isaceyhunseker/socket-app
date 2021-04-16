f7329e327fb0ca995f47663768e0c4b078572646
ceyhun <isaceyhunseker@gmail.com>
2021-04-16 10:50:19 +0300

    adds two way communication to command server

--- 01 command-server/client.c
+++ 01 command-server/client.c
@@ -7,14 +7,13 @@
 #include <arpa/inet.h>
 
 #define PORT 2323
-#define BUFFER_LENGHT 1024
+#define BUFFER_LENGHT 10
+#define help "help"
 
 void main(){
-	
 	int clientSocket;
 	struct sockaddr_in serverAddr;
-	char msgBuffer[BUFFER_LENGHT];
-    char help[] = "help";
+	char msgBuffer[BUFFER_LENGHT], rcvBuffer[BUFFER_LENGHT];
 	clientSocket = socket(AF_INET, SOCK_STREAM, 0);
 	printf("[+]Client Socket Created Sucessfully.\n");
 
@@ -25,17 +24,21 @@ void main(){
 
 	connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
 	printf("[+]Connected to Server.\n");
+	memset(rcvBuffer, '\0', sizeof(char)*BUFFER_LENGHT);
 
 	while(1) {
 		printf("Enter command:(help to see all commands) ");
-		fgets(msgBuffer, 1024, stdin);
-        if(strcmp(msgBuffer,help) == 0){
-            printf("you got it");
-        }
+		fgets(msgBuffer, BUFFER_LENGHT, stdin);
+
 		send(clientSocket, msgBuffer, strlen(msgBuffer), 0);
-			if(msgBuffer[0] == 'q' && msgBuffer[1] == 'u'){
-				break;
-			}
+		if(msgBuffer[0] == 'q' && msgBuffer[1] == 'u')
+			break;
+		if(strncmp(msgBuffer,help,sizeof(help)) == 0){
+        	printf("you got it");
+        }
+		recv(clientSocket, rcvBuffer, BUFFER_LENGHT, 0);
+		printf("\nreceived answer:%s\n", rcvBuffer);
+		memset(rcvBuffer, '\0', sizeof(char)*BUFFER_LENGHT);
 	}
 	printf("[+]Closing the connection.\n");
 	shutdown(clientSocket, SHUT_RDWR);
